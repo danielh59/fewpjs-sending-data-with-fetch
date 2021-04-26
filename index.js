48d782c1a3851845b813ecf5fa295afec0175985
tak59 <tak59@live.com>
2021-04-26 11:40:33 -0400

    commit tree dbd68348d66212ca2261967164e6d850e4238c3c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+
+
+function submitData(user_name, user_email) {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: user_name,
+      email: user_email
+    })
+  });  
+  
+}
